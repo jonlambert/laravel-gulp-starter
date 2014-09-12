c2f2fec336f985c1fb15d2bd7151744f57e86dd3
Jon Lambert <jonlambert94@googlemail.com>
2014-09-12 12:57:03 +0100

    Added 'watch' to Gulp.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,12 @@ var gulp = require('gulp');
 var mainBowerFiles = require('main-bower-files');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
 
+/**
+ * Grab all bower main files and concat them into all.js.
+ * Shift this over to public/lib/js.
+ */
 gulp.task('build:bower', function() {
     gulp.src(mainBowerFiles())
         .pipe(concat('all.js'))
@@ -10,12 +15,19 @@ gulp.task('build:bower', function() {
         .pipe(gulp.dest('public/lib/js'))
 });
 
+/**
+ * Same as build:bower, but without uglify etc. to avoid obfuscating.
+ */
 gulp.task('dev-build:bower', function() {
     gulp.src(mainBowerFiles())
         .pipe(concat('all.js'))
         .pipe(gulp.dest('public/lib/js'))
 });
 
+/**
+ * Move CSS files over to public/lib/css
+ * Uglify and generally compress.
+ */
 gulp.task('build:css', function() {
     gulp.src('assets/css/**/*.css')
         .pipe(uglify())
@@ -33,3 +45,7 @@ gulp.task('dev-build:css', function() {
 
 gulp.task('build', ['build:css', 'build:bower']);
 gulp.task('dev-build', ['dev-build:css', 'dev-build:bower']);
+
+gulp.task('watch', function() {
+    watch('assets/css/**/*.css', ['dev-build:css']);
+});
